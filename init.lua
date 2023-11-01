f22417794cc1afa32602ddafd538891b3b13c426
dai <daichi.iphone@icloud.com>
2023-11-01 13:52:47 +0900

    不要なコメント削除

--- init.lua
+++ init.lua
@@ -1,9 +1,5 @@
 require("my")
 
--- require('im_select').setup {
---     default_im_select = "com.google.inputmethod.Japanese.Roman"
--- }
-
 -- lazy.nvimのインストール
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
